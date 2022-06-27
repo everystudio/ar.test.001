71b8752b79a0f8503f35fea490cecb1a01a8bc55
watanabe@marida <watanabe.everystudio@gmail.com>
2022-06-27 20:46:56 +0900

    プロジェクト追加

---
+++ Assets/ShowTrackingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTrackingInfo : MonoBehaviour
+{
+    [SerializeField] private Camera m_targetCamera;
+    private Text m_txtInfo;
+
+    void Start()
+    {
+        m_txtInfo = GetComponent<Text>();
+    }
+    void Update()
+    {
+        m_txtInfo.text = $"{m_targetCamera.transform.position.ToString()}\n" +
+            $"{m_targetCamera.transform.rotation.ToString()}";
+    }
+}

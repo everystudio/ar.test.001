1e620ee9b034836531f3448d024bf81e48fddf7f
watanabe@marida <watanabe.everystudio@gmail.com>
2022-06-30 21:27:25 +0900

    ボール投げる処理を追加

---
+++ Assets/BallThrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrow : MonoBehaviour
+{
+    public GameObject m_prefBall;
+    public Transform m_transformCamera;
+
+    void Update()
+    {
+        if (0 < Input.touchCount)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Rigidbody rigidbodyBall = Instantiate(m_prefBall).GetComponent<Rigidbody>();
+                rigidbodyBall.transform.position = m_transformCamera.position;
+                rigidbodyBall.velocity = m_transformCamera.forward * 10f;
+            }
+        }
+    }
+}

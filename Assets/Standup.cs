f25e93b0eac0d5e2578e649bdc3bd72e8fbf13f0
watanabe@marida <watanabe.everystudio@gmail.com>
2022-07-03 17:16:56 +0900

    モデルを立たせる

---
+++ Assets/Standup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Standup : MonoBehaviour
+{
+    public ARRaycastManager m_raycastMangaer;
+    public GameObject m_prefCharacter;
+
+    void Update()
+    {
+        if (0 < Input.touchCount)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                List<ARRaycastHit> hitList = new List<ARRaycastHit>();
+                if (m_raycastMangaer.Raycast(touch.position, hitList, TrackableType.All))
+                {
+                    Pose hitPose = hitList[0].pose;
+                    Instantiate(m_prefCharacter, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+    }
+}
